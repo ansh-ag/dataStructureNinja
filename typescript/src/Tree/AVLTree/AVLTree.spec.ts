5266086f73c6afaee588fa89a77089e3ccbc9105
Anshul Rasiwasia <anshul.r@protonmail.com>
2021-07-30 22:38:47 +0530

    Update AVLTree.spec.ts

--- typescript/src/Tree/AVLTree/AVLTree.spec.ts
+++ typescript/src/Tree/AVLTree/AVLTree.spec.ts
@@ -1,9 +1,9 @@
 import { AVLTree } from "./AVLTree";
 
 describe("AVL Tree cases :",()=>{
-    const tree = new AVLTree();
-
+    
     it("should insert a new node", ()=>{
+        const tree = new AVLTree();
         tree.insert(5);
         expect(tree.parentNode.value).toEqual(5);
         tree.insert(2);
@@ -13,6 +13,9 @@ describe("AVL Tree cases :",()=>{
     })
 
     it("should do a left left rotation on parent",()=>{
+        const tree = new AVLTree();
+
+        
         /**
          * let's say there are three nodes 
          * C <- B <- A, this should now be C <- B -> A
@@ -30,6 +33,8 @@ describe("AVL Tree cases :",()=>{
 
     })
     it("should do a left left rotation on child",()=>{
+        const tree = new AVLTree();
+
         /**
          * let's say there are four nodes 
          * C <- B <- A <-X, this should now be C <- B -> A, where B <- X
@@ -41,10 +46,10 @@ describe("AVL Tree cases :",()=>{
         tree.insert(3);
         tree.insert(1);
         
-        //if balanced, should be 1 <- 3 -> 5, where 3 <- 8
-        expect(tree.parentNode.value).toEqual(8);
+        //if balanced, should be 1 <- 3 <- 5 -> 8
+        expect(tree.parentNode.right.value).toEqual(8);
         expect(tree.parentNode.left.value).toEqual(3);
-        expect(tree.parentNode.left.right.value).toEqual(5);
+        expect(tree.parentNode.value).toEqual(5);
 
     })
 })
